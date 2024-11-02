64ec5dcf9ace123e6c0f6c693433beeb39b3ded7
Muyang Chen <chenmuyangduoduo@126.com>
2024-11-02 16:38:31 -0700

    02-03-setter-injection

---
+++ 02-spring-boot-spring-core/01-constructor-injection/src/main/java/com/example/springcodedemo/common/CricketCoach.java
@@ -0,0 +1,19 @@
+package com.example.springcodedemo.common;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @descriptions:
+ * @author: cmy
+ * @date: 2024/11/2 15:54
+ * @version: 1.0
+ */
+
+@Component
+public class CricketCoach implements Coach{
+
+    @Override
+    public String getDailyWorkout() {
+        return "practice fast bowling for 15 minutes!!!!!!";
+    }
+}
