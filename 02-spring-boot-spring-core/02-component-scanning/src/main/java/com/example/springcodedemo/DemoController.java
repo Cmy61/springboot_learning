3457c81aa348d8109e01632017cfce9d9e8930e3
Muyang Chen <chenmuyangduoduo@126.com>
2024-11-02 16:19:39 -0700

    02-02-component-scanning

---
+++ 02-spring-boot-spring-core/02-component-scanning/src/main/java/com/example/springcodedemo/DemoController.java
@@ -0,0 +1,30 @@
+package com.example.springcodedemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @descriptions:
+ * @author: cmy
+ * @date: 2024/11/2 15:58
+ * @version: 1.0
+ */
+@RestController
+public class DemoController {
+    //define a private field for the dependency
+    private Coach myCoach;
+
+    //define a constrctor for dependency injection
+    @Autowired
+    public DemoController(Coach theCoach)
+    {
+        myCoach=theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout()
+    {
+        return myCoach.getDailyWorkout();
+    }
+}
