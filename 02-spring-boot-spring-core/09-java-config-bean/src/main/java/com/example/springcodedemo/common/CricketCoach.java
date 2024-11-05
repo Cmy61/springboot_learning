3f9fa8132f4688ef2e31120f2720f1a05dff7340
Muyang Chen <chenmuyangduoduo@126.com>
2024-11-05 10:25:41 -0800

    bean lifestyle_bean java config

--- 02-spring-boot-spring-core/09-java-config-bean/src/main/java/com/example/springcodedemo/common/CricketCoach.java
+++ 02-spring-boot-spring-core/09-java-config-bean/src/main/java/com/example/springcodedemo/common/CricketCoach.java
@@ -1,5 +1,7 @@
 package com.example.springcodedemo.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
@@ -13,11 +15,22 @@ import org.springframework.stereotype.Component;
  */
 
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CricketCoach implements Coach{
     public CricketCoach()
     {
         System.out.println("In constructor: "+getClass().getSimpleName());
+    }
+    //define our init method
+    @PostConstruct
+    public void doMyStartupStuff()
+    {
+        System.out.println("In doMyStartupStuff():"+getClass().getSimpleName());
+    }
+    //define our destroy method
+    @PreDestroy
+    public void doMyCleanupStuff()
+    {
+        System.out.println("In doMyCleanupStuff():"+getClass().getSimpleName());
     }
     @Override
     public String getDailyWorkout() {
--- 02-spring-boot-spring-core/09-java-config-bean/src/main/java/com/example/springcodedemo/common/TennisCoach.java
+++ 02-spring-boot-spring-core/09-java-config-bean/src/main/java/com/example/springcodedemo/common/TennisCoach.java
@@ -1,22 +1,21 @@
 package com.example.springcodedemo.common;
 
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 /**
  * @descriptions:
  * @author: cmy
- * @date: 2024/11/4 17:31
+ * @date: 2024/11/4 17:30
  * @version: 1.0
  */
 @Component
-public class TrackCoach implements Coach{
-    public TrackCoach()
+public class TennisCoach implements Coach{
+    public TennisCoach()
     {
         System.out.println("In constructor: "+getClass().getSimpleName());
     }
     @Override
     public String getDailyWorkout() {
-        return "Run a hard 5k!";
+        return "Practice your backhand volley";
     }
 }
